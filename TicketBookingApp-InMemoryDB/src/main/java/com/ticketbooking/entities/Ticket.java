51df98311c9179fd0c69b98ae2343fee488862e3
Piyush Singh <Piyush.Singh@utc.com>
2020-05-24 17:52:28 +0530

    Changes in Model Class

--- TicketBookingApp-InMemoryDB/src/main/java/com/ticketbooking/entities/Ticket.java
+++ TicketBookingApp-InMemoryDB/src/main/java/com/ticketbooking/entities/Ticket.java
@@ -14,34 +14,32 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-
-
 @Entity
-@Table(name="ticket_table")
+@Table(name = "ticket_table")
 @Setter
 @Getter
 @ToString
 @NoArgsConstructor
 public class Ticket {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name="ticket_id")
+	@Column(name = "ticket_id")
 	private Integer ticketId;
-	
-	@Column(name="passenger_name")
+
+	@Column(name = "passenger_name")
 	private String passengerName;
-	
-	@Column(name="source_station")
+
+	@Column(name = "source_station")
 	private String sourceStation;
-	
-	@Column(name="destination_station")
+
+	@Column(name = "destination_station")
 	private String destinataionStation;
-	
-	@Column(name="journey_date")
+
+	@Column(name = "journey_date")
 	private Date dateOfJourney;
-	
-	@Column(name="email_id")
+
+	@Column(name = "email_id")
 	private String email;
 
 	public Ticket(String passengerName, String sourceStation, String destinataionStation, Date dateOfJourney,
@@ -53,11 +51,5 @@ public class Ticket {
 		this.dateOfJourney = dateOfJourney;
 		this.email = email;
 	}
-	
-	
-	
-	
-	
-	
 
 }
